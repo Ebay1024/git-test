93d31d79fef4d42cda697ba248b745b1bbb1bbb6
EbayO <18192721208@163.com>
2019-10-16 21:40:43 +0800

    添加第一个方法

--- src/main/java/com/wu/git/GitDemo.java
+++ src/main/java/com/wu/git/GitDemo.java
@@ -4,4 +4,9 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git test");
     }
+    public void firstMethod(){
+        int a = 0;
+        a += 10;
+        System.out.println(a);
+    }
 }
